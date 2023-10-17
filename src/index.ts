06ceeadbe4848ef80ddf6d10c813326b5996f81e
itzdrli <liletian1231@outlook.com>
2023-10-17 17:23:18 +0000

    updated log

--- src/index.ts
+++ src/index.ts
@@ -11,9 +11,9 @@ export function apply(ctx: Context,config: Config) {
     const random = new Random(() => Math.random())
     let banTime = random.int(60000, 43200000)
     banTime = Math.floor(banTime / 1000) * 1000
-    ctx.logger('banme').info(`Banned ${session.userId} for ${banTime}ms`)
     await session.bot.muteGuildMember(session.guildId, session.userId, banTime)
     const rly = banTime / 1000
+    ctx.logger('banme').info(`Banned ${session.platform} - ${session.userId} for ${banTime}ms(${rly}s)`)
     return `已禁言抖M ${session.username} ${rly} 秒`
   })
 }
